13f99f25f7c2ab05fd5b89a41201228ea200d5f4
ZOI <zoizoichannel@gmail.com>
2020-08-08 18:41:18 +0900

    ControlSpreadFire(id, value) 追加

--- src/main/java/jp/hack/minecraft/blockguard/logic/BlockGuardLogic.java
+++ src/main/java/jp/hack/minecraft/blockguard/logic/BlockGuardLogic.java
@@ -1,9 +1,12 @@
 package jp.hack.minecraft.blockguard.logic;
 
 import jp.hack.minecraft.blockguard.core.Region;
+import jp.hack.minecraft.blockguard.core.RegionManager;
 import jp.hack.minecraft.blockguard.core.RegionPlugin;
 import jp.hack.minecraft.blockguard.core.Vectors;
+import org.bukkit.GameRule;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -19,5 +22,12 @@ public class BlockGuardLogic extends Region implements Listener {
         if(isWorking()) {
             e.setCancelled(true);
         }
+    }
+
+    public void ControlSpreadFire(String id, boolean value){
+        String worldName = RegionManager.getInstance().findRegion(id).getVectors().getWorldName();
+        World world = getPlugin().getServer().getWorld(worldName);
+
+        world.setGameRule(GameRule.DO_FIRE_TICK, value);
     }
 }
